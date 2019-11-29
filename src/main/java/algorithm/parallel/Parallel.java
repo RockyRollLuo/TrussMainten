13ff3da6c24b416a739ae53ac7ea5841267f4e4c
RockyRoll <luoqi4110217@hotmail.com>
2019-11-29 11:06:04 +0800

    MultiThread

---
+++ src/main/java/algorithm/parallel/Parallel.java
@@ -0,0 +1,16 @@
+package algorithm.parallel;
+
+import java.util.concurrent.ExecutorService;
+
+public class Parallel {
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
--- src/main/java/algorithm/parallel/SupTrussInsertion.java
+++ src/main/java/algorithm/parallel/SupTrussInsertion.java
@@ -1,14 +1,19 @@
-package algorithm;
+package algorithm.parallel;
 
 import util.Edge;
 import util.Graph;
 
 import java.util.Hashtable;
 
-public class SupTrussInsertionThread extends Thread {
+public class SupTrussInsertion extends Thread {
     private Graph graph;
     private Edge e0;
     private Hashtable<Edge, Integer> trussMap; //include edges in graph and e0
+
+
+
+
+
 
     @Override
     public void run() {
